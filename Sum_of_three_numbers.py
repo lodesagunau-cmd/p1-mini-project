7bbdb880ee26d34da083d503bb5bd37e812cc798
lodesagunau-cmd <lode.sagun.au@phinmaed.com>
2026-08-20 23:07:02 +0800

    Add program to calculate sum of three numbers

---
+++ Sum_of_three_numbers.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+total = num1+num2+num3
+
+print("The sum of the three numbers is:",total)			
+
+
+Enter first number: 8
+Enter second number: 18
+Enter third number: 18
+The sum of the three numbers is: 44
+
+[Program finished]
